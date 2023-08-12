3b91e2872f0a514354c5b268852c0f56abc523e5
Obia Gift <gyvfted3@gmail.com>
2023-08-11 20:31:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int d;
+
+	for (d = '0'; d <= '0'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
