7a5be06b56cafdcd5aca802694da794f9f172eb4
Obia Gift <gyvfted3@gmail.com>
2023-08-15 16:37:34 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,13 @@
 
 int print_last_digit(int n)
 {
-int last_digit;
-if (n < 0)
+int last;
+
+last = n % 10;
+if (last < 0)
 {
-n = -n;
+last = last * -1;
 }
-last_digit = n % 10;
-_putchar(last_digit + '0');
-return (last_digit);
+_putchar(last + '0');
+return (last);
 }
