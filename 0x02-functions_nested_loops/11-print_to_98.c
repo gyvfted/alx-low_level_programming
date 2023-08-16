158f28795eb719e6a30fce444bd940592c81c2f2
Obia Gift <gyvfted3@gmail.com>
2023-08-16 15:27:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,20 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	if (n <= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (; n <= 98; n++)
 		{
-			printf("%d", i);
-			if (i == 98)
-				continue;
-			printf(',');
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
-	else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i == 98)
-				continue;
-			printf(", ");
-		}
-	}
-	printf('\n');
 }
