cc9642ab58eb9d904f34d0ff27e611a05382fbd4
Obia Gift <gyvfted3@gmail.com>
2023-08-16 14:50:33 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int prev1, prev2, prev3, sum;
+
+	sum = 0;
+	prev1 = 1;
+	prev2 = 2;
+	while (prev1 < 4000000)
+	{
+		if (prev1 % 2 == 0)
+			sum += prev1;
+		prev3 = prev2;
+		prev2 = prev1 + prev2;
+		prev1 = prev3;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
