61f9509eae989e4fe654cef9b740c9a0a58304f0
Obia Gift <gyvfted3@gmail.com>
2023-08-21 12:27:25 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length o @str.
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; *s++; size++)
+	{
+	}
+	return (size);
+}
