b435248e963a91ae97f58ed4f95d41c662c40e1b
Obia Gift <gyvfted3@gmail.com>
2023-08-24 12:33:02 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,9 +3,10 @@
 /**
  * _strcmp - Compares two strings
  * @s1: First string to be compared
- * @s2: Second string to be compared 
+ * @s2: Second string to be compared
  * Return: Differnce between the first unmatching char
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
