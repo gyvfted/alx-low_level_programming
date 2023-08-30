bae766d76fc842ee1570dd704c6a651eaf3a1e7a
Obia Gift <gyvfted3@gmail.com>
2023-08-30 13:28:56 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: The base
+ * @y: The power
+ * Return: The value of the exponential
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
