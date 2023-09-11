15aaed5479e93f3bd458ca9d831ff425a8b688ab
Obia Gift <gyvfted3@gmail.com>
2023-09-11 14:46:03 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
- * struct dog - Struct of a dog's basic information
- * @name: Name of dog
- * @age: Age of dog
- * @owner: Owner of dog
- * Description: struct of a dog storing its name, its age
- * and the name of its owner.
+ * struct dog - A struct that stores some information of a dog
+ * @name: Name of the dog
+ * @age: Age of the Dog
+ * @owner: Owner of the dog
+ * Description: A struct called "dog" that stores its name,
+ * age and owner
  */
 
 struct dog
@@ -23,9 +17,17 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
