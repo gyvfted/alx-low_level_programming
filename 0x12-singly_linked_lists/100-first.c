6d68a1f8abb7305f6f31588baa2e7bd5650c4416
Gift Obia <gyvfted3@gmail.com>
2023-09-21 17:39:48 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * first - prints a sentence before the main
  * function is to be executed
  */
-
+void __attribute__ ((constructor)) bmain()
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
