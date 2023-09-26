5392e5a94ff0df33a12c31f70bd9d55195c2c028
Gift Obia <gyvfted3@gmail.com>
2023-09-26 10:55:38 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - Deletes the node at index,
+ * of a listint_t linked list
  * @head: Head of the linked list
  * @index: Index of list of nodes to be deleted
  *
