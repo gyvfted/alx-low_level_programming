30fc8138d60ff7def95d798764ed891b69e4f516
Gift Obia <gyvfted3@gmail.com>
2023-10-02 13:49:41 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile- Reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters the
+ * function should read and print
+ * Return: The actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
