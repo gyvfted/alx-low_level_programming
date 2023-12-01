64d844f4bff1bb8611cc29bcf81b093fcf13c1ce
Gift Obia <gyvfted3@gmail.com>
2023-12-01 11:30:18 -0800

    hash_table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -188,7 +188,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 /**
  * shash_table_delete - Deletes a sorted hash table.
  * @ht: A pointer to the sorted hash table.
- * Return: Void
+ * Return: Void.
  */
 void shash_table_delete(shash_table_t *ht)
 {
